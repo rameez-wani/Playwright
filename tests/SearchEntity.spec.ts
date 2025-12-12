2d7ee717082cf92dae4f48e102b8e7d87504bb67
rameez <wanirameez97@gmail.com>
2025-12-12 15:46:47 +0530

    search test case issue fixed

--- tests/SearchEntity.spec.ts
+++ tests/SearchEntity.spec.ts
@@ -10,9 +10,11 @@ test('Search entity', async ({ page }) => {
   await page.waitForTimeout(10000);
    await page.locator('#globalSearchBox').click();
    await page.locator('#globalSearchBox').fill(created.randomText);
+   await page.locator('#globalSearchBox').fill(created.randomText);
+   await page.locator('#globalSearchBox').press('Enter');
    await page.waitForTimeout(5000);
-   await expect(page.locator(created.randomText)).toBeVisible();
-   await expect(page.locator(created.randomPhone)).toBeVisible();
-   await expect(page.locator(created.email)).toBeVisible();
+   await expect(page.getByText(created.randomText)).toBeVisible();
+   await expect(page.getByText(created.randomPhone)).toBeVisible();
+   await expect(page.getByText(created.email)).toBeVisible();
   await page.waitForTimeout(5000);
 });
